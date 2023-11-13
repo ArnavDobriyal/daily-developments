f2e428a3a96bc7ca5fd99c302fefaa21e82553f1
arnav-dobriyal <143323143+TeamCIP@users.noreply.github.com>
2023-11-14 02:32:28 +0530

    Create coverter.py

---
+++ coverter.py
@@ -0,0 +1,25 @@
+import fitz  # PyMuPDF
+from docx import Document
+
+def pdf_to_text(pdf_path):
+    doc = fitz.open(pdf_path)
+    text = ""
+    for page_num in range(doc.page_count):
+        page = doc[page_num]
+        text += page.get_text()
+    return text
+
+def text_to_word(text, output_path):
+    doc = Document()
+    doc.add_paragraph(text)
+
+    doc.save(output_path)
+
+if __name__ == "__main__":
+    pdf_path = "resume.pdf"
+    output_path = "resume.docx"
+
+    text_content = pdf_to_text(pdf_path)
+    text_to_word(text_content, output_path)
+
+    print(f"Conversion complete. Word file saved to {output_path}")
