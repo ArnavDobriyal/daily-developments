3df41700ccdd42c6e6ba8751deee7e923023e904
arnav-dobriyal <143323143+TeamCIP@users.noreply.github.com>
2023-11-18 23:20:16 +0530

    Update youtubedownloader.py

--- youtube-downloder/youtubedownloader.py
+++ youtube-downloder/youtubedownloader.py
@@ -3,4 +3,4 @@ a=input("input url") # the url of video
 yt=YouTube(a)
 stream = yt.streams.get_highest_resolution()
 stream.download(output_path='C:\\Users\\dobri\\Videos\\downloadedvideo')
-print(yt.title)
+print(yt.title)#details
